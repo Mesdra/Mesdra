36a81709ad31ab9d65bc24b13c8217f9617d44e1
vini <vini@DESKTOP-QA0JORM>
2018-06-25 22:05:10 -0300

    comit

--- Projeto Computaçao grafica/src/Testes/testesJanela.java
+++ Projeto Computaçao grafica/src/Testes/testesJanela.java
@@ -35,21 +35,21 @@ public class testesJanela extends JFrame {
 	 */
 	public void paint(Graphics g) {
 		
-		calculaBezie(0, 50, 80, 0, 50, 30, 0, 50, 50);
+		calculaBezie(0,0,0,50,50,0,30,30,0,100,100,0);
 		
 		
 		g.drawPolygon(x, y, 500);
 	}
 	
-	public void calculaBezie(int x1,int x2,int x3,int y1,int y2,int y3,int z1,int z2,int z3) {
+	public void calculaBezie(int x1,int y1,int z1,int x2,int y2,int z2,int x3,int y3,int z3,int x4,int y4,int z4) {
 		float t = 0;
 	
 		for (int i = 0; i < 100; i++) {
 			t += 0.1;
 			
-			int Tx = (int) (Math.pow((1-t),2)*x1 + 2*t*(1-t)*x2+2*t*x3);
-			int Ty = (int) (Math.pow((1-t),2)*y1 + 2*t*(1-t)*y2+2*t*y3);
-			int Tz = (int) (Math.pow((1-t),2)*z1 + 2*t*(1-t)*z2+2*t*z3);
+			int Tx = (int) (Math.pow((1-t),3)*x1 + 3*t*Math.pow((1-t),2)*x2+Math.pow((3*t), 2)*(1-t)*x3+Math.pow(t,3)*x4);
+			int Ty = (int) (Math.pow((1-t),3)*y1 + 3*t*Math.pow((1-t),2)*y2+Math.pow((3*t), 2)*(1-t)*y3+Math.pow(t,3)*y4);
+			int Tz = (int) (Math.pow((1-t),3)*z1 + 3*t*Math.pow((1-t),2)*z2+Math.pow((3*t), 2)*(1-t)*z3+Math.pow(t,3)*z4);
 		
 			this.x[i] = Tx;
 			this.y[i] = Ty;
